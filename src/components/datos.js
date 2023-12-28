2ee8acd6811157c8174c52a76b578080f27c6111
daniel-aizcorbe <daizcorbe@frba.utn.edu.ar>
2023-12-28 19:28:39 -0300

    add datos de evoluciones anteriores para la lista

---
+++ src/components/datos.js
@@ -0,0 +1,245 @@
+const evoluciones = [
+    {
+      id: 1,
+      hora: "22:15",
+      fecha: "22/09/2023",
+      sector: "HEMODINAMIA"
+    },
+    {
+      id: 2,
+      hora: "22:00",
+      fecha: "22/09/2023",
+      sector: "HEMODINAMIA"
+    },
+    {
+      id: 3,
+      hora: "20:00",
+      fecha: "22/09/2023",
+      sector: "HEMODINAMIA"
+    },
+    {
+      id: 4,
+      hora: "22:15",
+      fecha: "23/09/2023",
+      sector: "HEMODINAMIA"
+    },
+    {
+      id: 5,
+      hora: "10:30",
+      fecha: "23/09/2023",
+      sector: "CIRUGÍA"
+    },
+    {
+      id: 6,
+      hora: "12:45",
+      fecha: "23/09/2023",
+      sector: "CIRUGÍA"
+    },
+    {
+      id: 7,
+      hora: "15:20",
+      fecha: "23/09/2023",
+      sector: "CIRUGÍA"
+    },
+    {
+      id: 8,
+      hora: "08:00",
+      fecha: "24/09/2023",
+      sector: "PEDIATRÍA"
+    },
+    {
+      id: 9,
+      hora: "10:45",
+      fecha: "24/09/2023",
+      sector: "PEDIATRÍA"
+    },
+    {
+      id: 10,
+      hora: "13:30",
+      fecha: "24/09/2023",
+      sector: "PEDIATRÍA"
+    },
+    {
+      id: 11,
+      hora: "09:15",
+      fecha: "25/09/2023",
+      sector: "CARDIOLOGÍA"
+    },
+    {
+      id: 12,
+      hora: "11:30",
+      fecha: "25/09/2023",
+      sector: "CARDIOLOGÍA"
+    },
+    {
+      id: 13,
+      hora: "14:00",
+      fecha: "25/09/2023",
+      sector: "CARDIOLOGÍA"
+    },
+    {
+      id: 14,
+      hora: "09:00",
+      fecha: "26/09/2023",
+      sector: "NEUROLOGÍA"
+    },
+    {
+      id: 15,
+      hora: "11:20",
+      fecha: "26/09/2023",
+      sector: "NEUROLOGÍA"
+    },
+    {
+      id: 16,
+      hora: "15:45",
+      fecha: "26/09/2023",
+      sector: "NEUROLOGÍA"
+    },
+    {
+      id: 17,
+      hora: "08:30",
+      fecha: "27/09/2023",
+      sector: "GINECOLOGÍA"
+    },
+    {
+      id: 18,
+      hora: "10:00",
+      fecha: "27/09/2023",
+      sector: "GINECOLOGÍA"
+    },
+    {
+      id: 19,
+      hora: "12:15",
+      fecha: "27/09/2023",
+      sector: "GINECOLOGÍA"
+    },
+    {
+      id: 20,
+      hora: "14:45",
+      fecha: "27/09/2023",
+      sector: "GINECOLOGÍA"
+    },
+    {
+      id: 21,
+      hora: "16:30",
+      fecha: "28/09/2023",
+      sector: "DERMATOLOGÍA"
+    },
+    {
+      id: 22,
+      hora: "09:45",
+      fecha: "28/09/2023",
+      sector: "DERMATOLOGÍA"
+    },
+    {
+      id: 23,
+      hora: "12:00",
+      fecha: "28/09/2023",
+      sector: "DERMATOLOGÍA"
+    },
+    {
+      id: 24,
+      hora: "14:20",
+      fecha: "28/09/2023",
+      sector: "DERMATOLOGÍA"
+    },
+    {
+      id: 25,
+      hora: "16:45",
+      fecha: "29/09/2023",
+      sector: "OTORRINOLARINGOLOGÍA"
+    },
+    {
+      id: 26,
+      hora: "10:10",
+      fecha: "29/09/2023",
+      sector: "OTORRINOLARINGOLOGÍA"
+    },
+    {
+      id: 27,
+      hora: "12:30",
+      fecha: "29/09/2023",
+      sector: "OTORRINOLARINGOLOGÍA"
+    },
+    {
+      id: 28,
+      hora: "15:00",
+      fecha: "29/09/2023",
+      sector: "OTORRINOLARINGOLOGÍA"
+    },
+    {
+      id: 29,
+      hora: "17:15",
+      fecha: "30/09/2023",
+      sector: "ENDOCRINOLOGÍA"
+    },
+    {
+      id: 30,
+      hora: "09:50",
+      fecha: "30/09/2023",
+      sector: "ENDOCRINOLOGÍA"
+    },
+    {
+      id: 31,
+      hora: "11:40",
+      fecha: "30/09/2023",
+      sector: "ENDOCRINOLOGÍA"
+    },
+    {
+      id: 32,
+      hora: "14:10",
+      fecha: "30/09/2023",
+      sector: "ENDOCRINOLOGÍA"
+    },
+    {
+      id: 33,
+      hora: "16:20",
+      fecha: "01/10/2023",
+      sector: "UROLOGÍA"
+    },
+    {
+      id: 34,
+      hora: "09:30",
+      fecha: "01/10/2023",
+      sector: "UROLOGÍA"
+    },
+    {
+      id: 35,
+      hora: "12:05",
+      fecha: "01/10/2023",
+      sector: "UROLOGÍA"
+    },
+    {
+      id: 36,
+      hora: "14:50",
+      fecha: "01/10/2023",
+      sector: "UROLOGÍA"
+    },
+    {
+      id: 37,
+      hora: "16:00",
+      fecha: "02/10/2023",
+      sector: "GASTROENTEROLOGÍA"
+    },
+    {
+      id: 38,
+      hora: "09:00",
+      fecha: "02/10/2023",
+      sector: "GASTROENTEROLOGÍA"
+    },
+    {
+      id: 39,
+      hora: "11:20",
+      fecha: "02/10/2023",
+      sector: "GASTROENTEROLOGÍA"
+    },
+    {
+      id: 40,
+      hora: "15:45",
+      fecha: "02/10/2023",
+      sector: "GASTROENTEROLOGÍA"
+    }
+  ];
+  
+
+export {evoluciones}
