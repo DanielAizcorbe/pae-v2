6b9656826ef2f50db6c81278177d0f62398f0be3
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-04-19 20:46:30 -0300

    cambio en el icono del paciente

--- src/components/botones/IconoPaciente.js
+++ src/components/botones/IconoPaciente.js
@@ -4,11 +4,23 @@ import { styled } from "styled-components";
 
 const IconoPaciente = () => {
     return (
-        <Imagen src={medicoImagen} alt="icono paciente"/>
+        <Imagen>
+            <img
+                src={medicoImagen}
+                alt="icono paciente"
+                style={{
+                    width: "8rem",
+                    height: "8rem"
+                }}
+            />
+            <span style={{fontSize: "1.5rem" }}>
+                Juan
+            </span>
+        </Imagen>
     );
 }
 
-const Imagen = styled.img`
+const Imagen = styled.div`
     width: 9rem;
     height: 9rem;
     margin: 2.5rem 5rem;
@@ -18,6 +30,10 @@ const Imagen = styled.img`
     pointer-events: none;
     position: absolute;
     bottom: -125px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `;
 
 export { IconoPaciente }
