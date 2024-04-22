ed7240219a99be195b221bd32e67755a88892dd4
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-04-22 20:00:04 -0300

    Uso de uuidv4 para el id

--- src/components/utils/MapaMental/ListaMarcadores.js
+++ src/components/utils/MapaMental/ListaMarcadores.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ItemMarcadorDeshabilitado } from './ItemMarcadorDeshabilitado'
+const { v4: uuidv4 } = require('uuid');
 
 const ListaMarcadores = ({ marcadores }) => {
 
+    const id = uuidv4();
+
     return (
         <ol>
-            {marcadores.map(m => <ItemMarcadorDeshabilitado practica={m.text} id={m.text + m.id}/>)}
+            {marcadores.map(m => <ItemMarcadorDeshabilitado practica={m.text} id={id} />)}
         </ol>
     )
 }
