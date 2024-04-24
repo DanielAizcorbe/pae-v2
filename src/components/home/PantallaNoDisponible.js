ea18a799c73497730e66a14e572da794a0c1b210
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-04-24 17:56:05 -0300

    refactor

---
+++ src/components/home/PantallaNoDisponible.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Columns } from '../utils/containers/Containers'
+import { NavBar } from '../generales/NavBar'
+import { Image } from 'antd'
+import imagenNoData from "./addFileImage.svg"
+import Title from 'antd/es/typography/Title'
+
+export const PantallaNoDisponible = ({ seccion }) => {
+    return (
+        <Columns
+            elementPosition={"top-center"}
+        >
+            <NavBar
+                defaultSection={seccion}
+            />
+            <Columns
+                elementPosition={"top-center"}
+                width={"600px"}
+                padding={"3rem"}
+            >
+                <Title level={1}>
+                    ¡Ups!
+                </Title>
+                <Image
+                    src={imagenNoData}
+                    preview={false}
+                    style={{
+                        width: "350px",
+                        padding: "2rem"
+                    }}
+                />
+                <Title
+                    level={3}
+                    style={{
+                        textAlign: "center",
+
+                    }}
+                >
+                    Esta función no esta disponible en la versión de prueba 
+                </Title>
+            </Columns>
+        </Columns>
+    )
+}
