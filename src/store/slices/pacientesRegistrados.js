750826371ba7a0023b666c85e89efc0f7cf37945
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-04-26 22:27:57 -0300

    add documentacion de los objetos paciente

--- src/store/slices/pacientesRegistrados.js
+++ src/store/slices/pacientesRegistrados.js
@@ -1,11 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
+
+/**
+ * ? Los objetos paciente tienen las keys
+ * * nombre: STRING // ! en mayúsculas y sin tildes
+ * * apellidp: STRING // ! en mayúsculas y sin tildes
+ * * fechaNac: DD-MM-YYYY
+ */
 
 const reducers = {
     registrar: (state, action) => {
-
+        console.log(action.payload);
         state.push({id: uuidv4(), ...action.payload})
-
     },
 
 };
