a8847d7b503fa7a34e94b05a0c28440c8bdeb0fa
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2024-04-27 19:04:07 -0300

    fix problema de id en la lista de marcadores

--- src/components/utils/MapaMental/ListaMarcadores.js
+++ src/components/utils/MapaMental/ListaMarcadores.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { ItemMarcadorDeshabilitado } from './ItemMarcadorDeshabilitado'
-const { v4: uuidv4 } = require('uuid');
 
 const ListaMarcadores = ({ marcadores }) => {
 
-    const id = uuidv4();
-
     return (
         <ol>
-            {marcadores.map(m => <ItemMarcadorDeshabilitado practica={m.text} id={id} />)}
+            {marcadores.map(m => <ItemMarcadorDeshabilitado practica={m.text} id={m.id} />)}
         </ol>
     )
 }
