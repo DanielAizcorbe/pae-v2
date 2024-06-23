c86d1e63ff03de3df55e259893409a54227db66e
Daniel-Aizcorbe <daniel.aizcorbe1@gmail.com>
2024-06-23 02:57:24 -0300

    remove el aviso para el deploy

--- src/components/evolucion/Evolucion.js
+++ src/components/evolucion/Evolucion.js
@@ -1,41 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { EtapasEvolucion } from "./EtapasEvolucion";
 import { Columns } from "../utils/containers/Containers";
 import { Titulo } from "../utils/Titulos";
 import { NavBar } from "../generales/NavBar";
 import { useSelector } from "react-redux";
-import { message } from "antd";
-import { useDispatch } from "react-redux";
-import { limpiarMensaje } from "../../store/slices/avisosSlice";
 
 const Evolucion = () => {
 
     const paciente = useSelector(state => state.paciente);
-    const etapaCompletada = useSelector(state => state.avisos.message)
-
-    const [messageApi, contextHolder] = message.useMessage();
-    const dispatch = useDispatch();
-
-    const showAviso = () => {
-        if (etapaCompletada) {
-            messageApi.info({
-                content: "Se ha editado la etapa " + etapaCompletada + ", recuerda revisar las etapas siguientes",
-                duration: 10,
-                onClose: () => {
-                    dispatch(limpiarMensaje())
-                },
-                style: {
-                    marginTop: "60px",
-                    fontSize: "1rem"
-                }
-            });
-        }
-    }
-    useEffect(
-        () => {
-            showAviso()
-        }, [etapaCompletada]
-    );
 
     return (
         <Columns
@@ -52,7 +24,6 @@ const Evolucion = () => {
                 <Titulo texto={paciente.nombre + " " + paciente.apellido} />
             </Columns>
             <EtapasEvolucion />
-            {contextHolder}
         </Columns>
 
     );
